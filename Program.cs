c2623167ef6bc7c80ffca65109cdeb12a5e296bc
Brandon Reninger <brandonreninger@yahoo.com>
2020-06-24 22:24:21 -0700

    Visual Studio first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VSpracticeC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
